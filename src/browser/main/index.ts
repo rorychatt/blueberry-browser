2ed4326743265fb87d105a7ab26fc70ae88a8472
rorychatt <pavel.rinne@gmail.com>
2026-07-06 10:51:12 +0200

    fix: improve macOS application activation behavior for existing windows

--- src/browser/main/index.ts
+++ src/browser/main/index.ts
@@ -1,4 +1,4 @@
-import { BrowserWindow, app } from "electron";
+import { BaseWindow, app } from "electron";
 import { electronApp } from "@electron-toolkit/utils";
 import { Window } from "./components/Window";
 import { AppMenu } from "./components/Menu";
@@ -23,8 +23,11 @@ void app.whenReady().then(() => {
   app.on("activate", () => {
     // On macOS it's common to re-create a window in the app when the
     // Dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
+    if (BaseWindow.getAllWindows().length === 0) {
       mainWindow = createWindow();
+    } else {
+      mainWindow?.show();
+      mainWindow?.focus();
     }
   });
 });
